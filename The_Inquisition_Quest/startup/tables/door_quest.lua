1fe51f9cf328737efcd1c16e1a807decc1b3a24c
whitecrixu <63555106+whitecrixu@users.noreply.github.com>
2023-10-25 17:02:40 +0100

    Create door_quest.lua

---
+++ The_Inquisition_Quest/startup/tables/door_quest.lua
@@ -0,0 +1,15 @@
+--[[
+
+Look README.md for look the reserved action/unique
+
+]]
+QuestDoorAction = {
+		[Storage.TheInquisition.Mission07] = {
+		itemId = false,
+		itemPos = { {x = 3150, y = 4162, z = 6} },
+	},
+}
+
+QuestDoorUnique = {
+	-- 
+}
