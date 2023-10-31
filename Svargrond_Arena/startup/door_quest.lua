f4872a7c2b2a353fa8fb0cb15739ea81f0581a03
whitecrixu <63555106+whitecrixu@users.noreply.github.com>
2023-10-31 14:05:43 +0000

    Adding NPC for quest & Add Svargrond Arena with Svargrond Town

---
+++ Svargrond_Arena/startup/door_quest.lua
@@ -0,0 +1,32 @@
+--[[
+
+Look README.md for look the reserved action/unique
+
+]]
+QuestDoorAction = {
+	-- Svargrond arena door
+	[Storage.SvargrondArena.PitDoor] = {
+		itemId = false,
+		itemPos = {
+			{ x = 5042, y = 5040, z = 7 },
+			{ x = 5042, y = 5041, z = 7 },
+			{ x = 5042, y = 5042, z = 7 },
+		},
+	},
+	[Storage.SvargrondArena.GreenhornDoor] = {
+		itemId = false,
+		itemPos = { { x = 5036, y = 5008, z = 7 } },
+	},
+	[Storage.SvargrondArena.ScrapperDoor] = {
+		itemId = false,
+		itemPos = { { x = 5036, y = 5001, z = 7 } },
+	},
+	[Storage.SvargrondArena.WarlordDoor] = {
+		itemId = false,
+		itemPos = { { x = 5036, y = 4994, z = 7 } },
+	},	
+},
+}
+QuestDoorUnique = {
+	-- 
+}
