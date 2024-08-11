2611fc8610b0e50d65f88c21197d26073c382f50
whitecrixu <63555106+whitecrixu@users.noreply.github.com>
2024-08-11 21:42:57 +0100

    Update reward.lua
    optimized code

--- The_Inquisition_Quest/actions/reward.lua
+++ The_Inquisition_Quest/actions/reward.lua
@@ -1,3 +1,4 @@
+-- Configuration table for rewards
 local rewards = {
 	[1300] = 8062,
 	[1301] = 8090,
@@ -10,18 +11,34 @@ local rewards = {
 	[1308] = 8026,
 }
 
+-- Action handler for inquisition rewards
 local inquisitionRewards = Action()
+
 function inquisitionRewards.onUse(player, item, fromPosition, target, toPosition, isHotkey)
+	-- Check if the player has already received the reward
 	if player:getStorageValue(Storage.TheInquisition.Reward) < 1 then
+		-- Update the player's storage values
 		player:setStorageValue(Storage.TheInquisition.Reward, 1)
 		player:setStorageValue(Storage.TheInquisition.Questline, 25)
-		player:setStorageValue(Storage.TheInquisition.Mission07, 5) -- The Inquisition Questlog- "Mission 7: The Shadow Nexus"
-		player:addItem(rewards[item.uid], 1)
-		player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You have found " .. ItemType(rewards[item.uid]):getName() .. ".")
+		player:setStorageValue(Storage.TheInquisition.Mission07, 5) -- Update quest log for "Mission 7: The Shadow Nexus"
+
+		-- Give the reward item to the player
+		local rewardItemId = rewards[item.uid]
+		if rewardItemId then
+			player:addItem(rewardItemId, 1)
+			player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "You have found " .. ItemType(rewardItemId):getName() .. ".")
+		else
+			player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "Something went wrong, reward item not found.")
+			return true
+		end
+
+		-- Grant the player achievements and outfit addons
 		player:addAchievement("Master of the Nexus")
+
+		-- Adding both addons at once
+		player:addOutfitAddon(288, 1) -- Male Outfit
 		player:addOutfitAddon(288, 2)
-		player:addOutfitAddon(288, 1)
-		player:addOutfitAddon(289, 1)
+		player:addOutfitAddon(289, 1) -- Female Outfit
 		player:addOutfitAddon(289, 2)
 	else
 		player:sendTextMessage(MESSAGE_EVENT_ADVANCE, "The chest is empty.")
@@ -29,7 +46,8 @@ function inquisitionRewards.onUse(player, item, fromPosition, target, toPosition
 	return true
 end
 
-for uniqueId, info in pairs(rewards) do
+-- Register the action for each unique ID in the rewards table
+for uniqueId in pairs(rewards) do
 	inquisitionRewards:uid(uniqueId)
 end
 
